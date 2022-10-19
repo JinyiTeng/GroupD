1d9623a23e9abddb8cb9ad385937bd6bc0968077
JinyiTeng <jinyiteng06@gmail.com>
2022-10-19 15:35:53 +0930

    AngularPracticeEmp

---
+++ GroupD_assignment/WebApplication3tierApp-master/AngularPractice/AngularPractice/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './employee/list.component';
+import { AddeditComponent } from './employee/addedit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    AddeditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
