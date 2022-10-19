666543517d2e2c968cf257b5b07c1f5d04727f48
kylethan <quangkhai3266@gmail.com>
2022-10-19 11:27:54 +0930

    .NET Web API - Adding controllers

---
+++ GroupD_assignment/EmployeeWebAPI/EmployeeWebAPI/Models/EmpDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeWebAPI.Models
+{
+    public class EmpDBContext : DbContext
+    {
+        public EmpDBContext(DbContextOptions<Empdbcontext> options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
