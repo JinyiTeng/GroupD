770e62b9c96ec16fb58b2ae5dd37752be6103cab
JinyiTeng <jinyiteng06@gmail.com>
2022-10-26 11:21:41 +0930

    stock migration

--- GroupD_assignment/2DataAccessLayer/Migrations/20221024160531_stock.cs
+++ GroupD_assignment/2DataAccessLayer/Migrations/20221024160531_stock.cs
@@ -8,12 +8,29 @@ namespace _2DataAccessLayer.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                   name: "Stock",
+                   columns: table => new
+                   {
+                       StockID = table.Column<int>(type: "int", nullable: false)
+                           .Annotation("SqlServer:Identity", "1, 1"),
+                       ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                       OrderAmount = table.Column<int>(type: "int", nullable: false),
+                       TotalOrderPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                   },
+                   constraints: table =>
+                   {
+                       table.PrimaryKey("PK_Stock", x => x.StockID);
+                   });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Staffs");
 
         }
+
+    
     }
 }
